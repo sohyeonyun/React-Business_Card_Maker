c3272b156b6aa4c4583e41988099d0e686b03680
sohyeonyun <wja06018@naver.com>
2022-03-12 15:47:50 +0900

    add login component

--- src/service/firebase.js
+++ src/service/firebase.js
@@ -1,12 +1,13 @@
 import firebase from 'firebase';
-import 'firebase/auth';
-import 'firebase/database';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
 };
 
+console.log(process.env.REACT_APP_FIREBASE_API_KEY);
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+export default firebaseApp;
